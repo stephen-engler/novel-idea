45b350f091b8c0a94e7fe0d7ee3b041d2087168b
Stephen Engler <sengler1989@gmail.com>
2018-04-04 18:17:07 -0500

    post works server side

--- server/routes/books.router.js
+++ server/routes/books.router.js
@@ -4,9 +4,18 @@ const pool = require('../modules/pool.js');
 
 router.post('/', (req,res)=>{
     console.log('in router/post response ', req.body);
+    let book = req.body;
+    let queryText = `INSERT INTO "books" ("title", "author", "year", "pages","rating") VALUES ($1, $2, $3, $4, $5);`;
+    pool.query(queryText,[book.title, book.author, book.year, book.pages, book.rating])
+        .then((response)=>{
+            res.sendStatus(200);
+        })
+        .catch((error)=>{
+            console.log('in router/post an error in query ', error);
+            res.sendStatus(500);
+        });
+
 });
-
-
 
 
 module.exports = router;
